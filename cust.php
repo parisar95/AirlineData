334ac97d60af1035de7aa843796600d12e0061b6
Zijie Zhu <l2c0b@lulu.ugrad.cs.ubc.ca>
2017-03-20 20:28:47 -0700

    refactored & implemented ticket searching

--- cust.php
+++ cust.php
@@ -1,5 +1,8 @@
 <html>
 <body>
+
+<table><tr valign="top"><td width="40%">
+
 <div id="flightSearch">
     <h2>Search For a Flight</h2>
     <b>Search by Dates</b>
@@ -26,9 +29,7 @@
                 <td><input type="number" name="numPass"></td>
             </tr>
             <tr>
-                <td>
-                    <button type="submit" value="submit" name="searchDateSubmit">Search</button>
-                </td>
+                <td><button type="submit" value="submit" name="searchDateSubmit">Search</button></td>
             </tr>
         </table>
     </form>
@@ -44,29 +45,43 @@
                 <td><input type="date" name="dDate"></td>
             </tr>
             <tr>
-                <td>
-                    <button type="submit" value="submit" name="searchFnoSubmit">Search</button>
-                </td>
+                <td><button type="submit" value="submit" name="searchFnoSubmit">Search</button></td>
             </tr>
         </table>
     </form>
 </div>
-</body>
-</html>
+
+
+<div id="reservationSearch">
+    <h2>Search For a Reservation</h2>
+    <form method="POST" action="cust.php">
+        <table>
+	    <tr>
+                <td>Ticket ID</td>
+                <td><input type="input" name="tid"></td>
+            </tr>
+	    <tr>
+                <td>Passenger ID</td>
+                <td><input type="input" name="pid"></td>
+            </tr>
+            <tr>
+                <td><button type="submit" value="submit" name="searchResSubmit">Search</button></td>
+            </tr>
+        </table>
+    </form>
+</div>
+
+</td><td>
 
 <?php
 
 	error_reporting(-1);
 	ini_set('display_errors',1);
 
+	date_default_timezone_set ('UTC');
+	$hasAtLeastOneField = false;
 
     	if (array_key_exists('searchDateSubmit', $_POST)) {
-
-		require('sqlfn.php');
-		$username = $_COOKIE['username'];
-		$password = $_COOKIE['password'];
-		$db_conn = dbConn($username, $password);
-		date_default_timezone_set ('UTC');
 
 		$dDate = "'".strtoupper(date('Y-m-d', strtotime($_POST['dDate'])))."'";
 		$depacode = "'".$_POST['depacode']."'";
@@ -74,54 +89,72 @@
 		$arracode = "'".$_POST['arracode']."'";
 		$numPass = "'".$_POST['numPass'];
 
-		if ($dDate === "'1970-01-01'") $dDate = 'dateflight';
-		if ($depacode === "''") $depacode = 'depacode';
-		if ($aDate === "'1970-01-01'") $aDate = 'arrdate';
-		if ($arracode === "''") $arracode = 'arracode';
+		if ($dDate === "'1970-01-01'")	$dDate = 'dateflight'; 		else $hasAtLeastOneField = true;
+		if ($depacode === "''")		$depacode = 'depacode';		else $hasAtLeastOneField = true;
+		if ($aDate === "'1970-01-01'")	$aDate = 'arrdate';		else $hasAtLeastOneField = true;
+		if ($arracode === "''")		$arracode = 'arracode';		else $hasAtLeastOneField = true;
 
-		$result = executePlainSQL("select * from flight where dateflight = $dDate and depacode = $depacode and arrdate = $aDate and arracode = $arracode");
-		OCICommit($db_conn);
-		dbDisconn($db_conn);
+		$query = "select * from flight where dateflight = $dDate and depacode = $depacode and arrdate = $aDate and arracode = $arracode";
 
         } else if (array_key_exists('searchFnoSubmit', $_POST)) {
-
-		require('sqlfn.php');
-		$username = $_COOKIE['username'];
-		$password = $_COOKIE['password'];
-		$db_conn = dbConn($username, $password);
-		date_default_timezone_set ('UTC');
 
 		$fno = "'".$_POST['fno']."'";
 		$dDate = "'".strtoupper(date('Y-m-d', strtotime($_POST['dDate'])))."'";
 
-		if ($fno === "''") $fno = 'fno';
-		if ($dDate === "'1970-01-01'") $dDate = 'dateflight';
+		if ($fno === "''")		$fno = 'fno'; 			else $hasAtLeastOneField = true;
+		if ($dDate === "'1970-01-01'")	$dDate = 'dateflight'; 		else $hasAtLeastOneField = true;
 
-		$result = executePlainSQL("select * from flight where dateflight = $dDate and fno = $fno");
-		OCICommit($db_conn);
-		dbDisconn($db_conn);
+		$query = "select * from flight where dateflight = $dDate and fno = $fno";
+		
+	} else if (array_key_exists('searchResSubmit', $_POST)) {
+
+		$tid = "'".$_POST['tid']."'";
+		$pid = "'".$_POST['pid']."'";
+
+		if ($tid === "''") $tid = 'tid';	else $hasAtLeastOneField = true;
+		if ($pid === "''") $pid = 'pid';	else $hasAtLeastOneField = true;
+
+		$query = "select * from flight where fno in (select fno from ticket where tid = $tid and pid = $pid)";
 		
 	} else {
 		return;
 	}
 
-	echo '<table><thead><td>Flight No.</td><td>Departure Airport</td><td>Arrival Airport</td><td>Departure Date</td><td>Arrival Date</td></thead>';
+	if ($hasAtLeastOneField) {
+
+		require('sqlfn.php');
+		$username = $_COOKIE['username'];
+		$password = $_COOKIE['password'];
+		$db_conn = dbConn($username, $password);
+		$result = executePlainSQL($query);
+		OCICommit($db_conn);
+		dbDisconn($db_conn);
+		
+		echo '<table border="1"><thead>'.
+			'<td><b>Flight No.</b></td>'.
+			'<td><b>Departure Airport</b></td>'.
+			'<td><b>Arrival Airport</b></td>'.
+			'<td><b>Departure Date</b></td>'.
+			'<td><b>Arrival Date</b></td>'.
+		     '</thead>';
 
 
-	while ($row = OCI_Fetch_Array($result, OCI_BOTH)) {
-	    	echo "<tr>";
-		echo "<td>" . $row[0] . "</td>";
-		echo "<td>" . $row[3] . "</td>";
-		echo "<td>" . $row[6] . "</td>";
-		echo "<td>" . $row[1] . "</td>";
-		echo "<td>" . $row[4] . "</td>";
-		echo "</tr>";
-	}      
+		while ($row = OCI_Fetch_Array($result, OCI_BOTH)) {
+		    	echo "<tr  align='center'>";
+			echo "<td>" . $row[0] . "</td>";
+			echo "<td>" . $row[3] . "</td>";
+			echo "<td>" . $row[6] . "</td>";
+			echo "<td>" . $row[1] . "</td>";
+			echo "<td>" . $row[4] . "</td>";
+			echo "</tr>";
+		}      
 	
-	echo '</table>';  
+		echo '</table>';
+	}  
 
 ?>
 
-<script>
+</td></tr>
 
-function 
+</body>
+</html>
